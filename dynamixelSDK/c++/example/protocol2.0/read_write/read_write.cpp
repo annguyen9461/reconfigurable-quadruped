93a6c4e4749db267cbed139a59e4f09a3a2af5ae
An Nguyen <annguyen2025@u.northwestern.edu>
2025-01-20 14:23:49 -0600

    cant move motors anymore. DRAFT

--- dynamixelSDK/c++/example/protocol2.0/read_write/read_write.cpp
+++ dynamixelSDK/c++/example/protocol2.0/read_write/read_write.cpp
@@ -32,30 +32,54 @@
 // Control table address
 #if defined(X_SERIES)
   #define ADDR_TORQUE_ENABLE          64
-  #define ADDR_GOAL_POSITION          116
-  #define ADDR_PRESENT_POSITION       132
-  #define MINIMUM_POSITION_LIMIT      0
-  #define MAXIMUM_POSITION_LIMIT      4095
+  #define ADDR_GOAL_POSITION          200
+  #define ADDR_PRESENT_POSITION       200
+  // #define MINIMUM_POSITION_LIMIT      0
+  // #define MAXIMUM_POSITION_LIMIT      4095
   #define BAUDRATE                    57600
 #endif
 
 // DYNAMIXEL Protocol Version
 #define PROTOCOL_VERSION  2.0
 
-// Motors attached to legs
-// Motor IDs: 1, 3, 5, 7, 9, 11
-#define MINIMUM_POSITION_LIMIT_INNER_LEG      0  
-#define MAXIMUM_POSITION_LIMIT_INNER_LEG      1000
-// Motor IDs: 2, 4, 6, 8, 10, 12
-#define MINIMUM_POSITION_LIMIT_OUTER_LEG      1212  
-#define MAXIMUM_POSITION_LIMIT_OUTER_LEG      2280  
+
+#define ADDR_GOAL_POSITION1 200
+#define ADDR_GOAL_POSITION2 200
+#define ADDR_GOAL_POSITION3 200
+
+#define ADDR_PRESENT_POSITION1 200
+#define ADDR_PRESENT_POSITION2 200
+#define ADDR_PRESENT_POSITION3 200
+
+// Motors attached to body
+// Motor IDs: 3, 6, 9, 12
+#define MIN_POSITION_LIMIT_MOTOR1      135  
+#define MAX_POSITION_LIMIT_MOTOR1      315
+
+// Motors moving leg up
+// Motor IDs: 2, 5, 8, 11
+#define MIN_POSITION_LIMIT_MOTOR2      223      // UP
+#define MAX_POSITION_LIMIT_MOTOR2      250      // DOWN
+
+// Motors folding blades
+// Motor IDs: 1, 4, 7, 10
+#define MIN_POSITION_LIMIT_MOTOR3      153  
+#define MAX_POSITION_LIMIT_MOTOR3      215  
 
 // DYNAMIXEL IDs
-#define LEG1_INNER_ID  1
-#define LEG1_OUTER_ID  2
+
+// KEEP THIS TO INITIALIZEE
+#define MOTOR1_ID  20
+#define MOTOR2_ID  20
+#define MOTOR3_ID  20
+
+// CHANGE THIS
+#define MOTOR1_ID  4
+#define MOTOR2_ID  20
+#define MOTOR3_ID  20
 
 // Port configuration
-#define DEVICENAME  "/dev/ttyUSB0"
+#define DEVICENAME  "/dev/ttyUSB1"
 
 #define TORQUE_ENABLE                   1
 #define TORQUE_DISABLE                  0
@@ -110,12 +134,15 @@ int main() {
   dynamixel::PacketHandler *packetHandler = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
 
   int dxl_comm_result = COMM_TX_FAIL;
-  int dxl1_goal_position[2] = {MINIMUM_POSITION_LIMIT_INNER_LEG, MAXIMUM_POSITION_LIMIT_INNER_LEG};
-  int dxl2_goal_position[2] = {MINIMUM_POSITION_LIMIT_OUTER_LEG, MAXIMUM_POSITION_LIMIT_OUTER_LEG};
+  int dxl1_goal_position[2] = {MIN_POSITION_LIMIT_MOTOR1, MAX_POSITION_LIMIT_MOTOR1};
+  int dxl2_goal_position[2] = {MIN_POSITION_LIMIT_MOTOR2, MAX_POSITION_LIMIT_MOTOR2};
+  int dxl3_goal_position[2] = {MIN_POSITION_LIMIT_MOTOR3, MAX_POSITION_LIMIT_MOTOR3};
 
   uint8_t dxl_error = 0;
-  int32_t dxl1_present_position = 0;
-  int32_t dxl2_present_position = 0;
+  // TODO: CHANGE PRESENT POSITION AS NEEDED
+  int32_t dxl1_present_position = 153;
+  int32_t dxl2_present_position = 315;
+  int32_t dxl3_present_position = 333;
 
   // Open port
   if (portHandler->openPort()) {
@@ -138,14 +165,21 @@ int main() {
   }
 
   // Enable Torque for both servos
-  dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, LEG1_INNER_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE, &dxl_error);
+  dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, MOTOR1_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE, &dxl_error);
   if (dxl_comm_result != COMM_SUCCESS) {
     printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
   } else if (dxl_error != 0) {
     printf("%s\n", packetHandler->getRxPacketError(dxl_error));
   }
 
-  dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, LEG1_OUTER_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE, &dxl_error);
+  dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, MOTOR2_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE, &dxl_error);
+  if (dxl_comm_result != COMM_SUCCESS) {
+    printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+  } else if (dxl_error != 0) {
+    printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+  }
+
+  dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, MOTOR3_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE, &dxl_error);
   if (dxl_comm_result != COMM_SUCCESS) {
     printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
   } else if (dxl_error != 0) {
@@ -158,26 +192,32 @@ int main() {
     if (getch() == ESC_ASCII_VALUE) break;
 
     // Write goal positions for both servos
-    dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, LEG1_INNER_ID, ADDR_GOAL_POSITION, dxl1_goal_position[index], &dxl_error);
-    dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, LEG1_OUTER_ID, ADDR_GOAL_POSITION, dxl2_goal_position[index], &dxl_error);
+    dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, MOTOR1_ID, ADDR_GOAL_POSITION1, dxl1_goal_position[index], &dxl_error);
+    dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, MOTOR2_ID, ADDR_GOAL_POSITION2, dxl2_goal_position[index], &dxl_error);
+    dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, MOTOR3_ID, ADDR_GOAL_POSITION3, dxl3_goal_position[index], &dxl_error);
 
     do {
       // Read present positions
-      dxl_comm_result = packetHandler->read4ByteTxRx(portHandler, LEG1_INNER_ID, ADDR_PRESENT_POSITION, (uint32_t*)&dxl1_present_position, &dxl_error);
-      dxl_comm_result = packetHandler->read4ByteTxRx(portHandler, LEG1_OUTER_ID, ADDR_PRESENT_POSITION, (uint32_t*)&dxl2_present_position, &dxl_error);
+      dxl_comm_result = packetHandler->read4ByteTxRx(portHandler, MOTOR1_ID, ADDR_PRESENT_POSITION1, (uint32_t*)&dxl1_present_position, &dxl_error);
+      dxl_comm_result = packetHandler->read4ByteTxRx(portHandler, MOTOR2_ID, ADDR_PRESENT_POSITION2, (uint32_t*)&dxl2_present_position, &dxl_error);
+      dxl_comm_result = packetHandler->read4ByteTxRx(portHandler, MOTOR3_ID, ADDR_PRESENT_POSITION3, (uint32_t*)&dxl2_present_position, &dxl_error);
 
       printf("[ID:%03d] GoalPos:%03d  PresPos:%03d [ID:%03d] GoalPos:%03d  PresPos:%03d\n",
-             LEG1_INNER_ID, dxl1_goal_position[index], dxl1_present_position,
-             LEG1_OUTER_ID, dxl2_goal_position[index], dxl2_present_position);
-    } while ((abs(dxl1_goal_position[index] - dxl1_present_position) > DXL_MOVING_STATUS_THRESHOLD) ||
-             (abs(dxl2_goal_position[index] - dxl2_present_position) > DXL_MOVING_STATUS_THRESHOLD));
+              MOTOR1_ID, dxl1_goal_position[index], dxl1_present_position,
+              MOTOR2_ID, dxl2_goal_position[index], dxl2_present_position,
+              MOTOR3_ID, dxl3_goal_position[index], dxl3_present_position);
+    } while ((abs(dxl1_goal_position[index] - dxl1_present_position) > DXL_MOVING_STATUS_THRESHOLD) 
+              ||  (abs(dxl2_goal_position[index] - dxl2_present_position) > DXL_MOVING_STATUS_THRESHOLD)
+              ||  (abs(dxl3_goal_position[index] - dxl3_present_position) > DXL_MOVING_STATUS_THRESHOLD)
+              );
 
     index = (index == 0) ? 1 : 0;
   }
 
   // Disable Torque
-  packetHandler->write1ByteTxRx(portHandler, LEG1_INNER_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE, &dxl_error);
-  packetHandler->write1ByteTxRx(portHandler, LEG1_OUTER_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE, &dxl_error);
+  packetHandler->write1ByteTxRx(portHandler, MOTOR1_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE, &dxl_error);
+  packetHandler->write1ByteTxRx(portHandler, MOTOR2_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE, &dxl_error);
+  packetHandler->write1ByteTxRx(portHandler, MOTOR3_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE, &dxl_error);
 
   // Close port
   portHandler->closePort();
