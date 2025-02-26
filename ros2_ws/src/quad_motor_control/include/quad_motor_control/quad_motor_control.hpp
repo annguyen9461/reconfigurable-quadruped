7bdd8763826aff149f47cdb6cd19a760b8bf5820
An Nguyen <annguyen2025@u.northwestern.edu>
2025-02-26 14:06:54 -0600

    CHECKPOINT can get and set 1 dynamixel motor with ros2

--- ros2_ws/src/quad_motor_control/include/quad_motor_control/quad_motor_control.hpp
+++ ros2_ws/src/quad_motor_control/include/quad_motor_control/quad_motor_control.hpp
@@ -8,18 +8,29 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rcutils/cmdline_parser.h"
 #include "dynamixel_sdk/dynamixel_sdk.h"
+#include "quad_interfaces/msg/set_position.hpp"
+#include "quad_interfaces/srv/get_position.hpp"
 
 class QuadMotorControl : public rclcpp::Node 
 {
 public:
+    using SetPosition = quad_interfaces::msg::SetPosition;
+    using GetPosition = quad_interfaces::srv::GetPosition;
+
     QuadMotorControl();
     virtual ~QuadMotorControl();
+
 private:
     dynamixel::PortHandler * portHandler;
     dynamixel::PacketHandler * packetHandler;
     dynamixel::GroupSyncWrite* groupSyncWrite;
 
     void initDynamixels();
+
+    rclcpp::Subscription<SetPosition>::SharedPtr set_position_subscriber_;
+    rclcpp::Service<GetPosition>::SharedPtr get_position_server_;
+
+    int present_position;
 };
 
 #endif  // QUAD_MOTOR_CONTROL_HPP_
--- ros2_ws/src/quad_motor_control/src/quad_motor_control.cpp
+++ ros2_ws/src/quad_motor_control/src/quad_motor_control.cpp
@@ -27,73 +27,73 @@ QuadMotorControl::QuadMotorControl() : Node("quad_motor_control")
     int8_t qos_depth = 0;
     this->get_parameter("qos_depth", qos_depth);
 
-    // const auto QOS_RKL10V =
-    //     rclcpp::QoS(rclcpp::KeepLast(qos_depth)).reliable().durability_volatile();
+    const auto QOS_RKL10V =
+        rclcpp::QoS(rclcpp::KeepLast(qos_depth)).reliable().durability_volatile();
 
     this->portHandler = dynamixel::PortHandler::getPortHandler(DEVICE_NAME);
     this->packetHandler = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
 
     this->initDynamixels();
 
-    // set_position_subscriber_ =
-    //     this->create_subscription<SetPosition>(
-    //     "set_position",
-    //     QOS_RKL10V,
-    //     [this](const SetPosition::SharedPtr msg) -> void
-    //         {
-    //             uint8_t dxl_error = 0;
+    set_position_subscriber_ =
+        this->create_subscription<SetPosition>(
+        "set_position",
+        QOS_RKL10V,
+        [this](const SetPosition::SharedPtr msg) -> void
+        {
+            uint8_t dxl_error = 0;
 
-    //             // Position Value of X series is 4 byte data.
-    //             // For AX & MX(1.0) use 2 byte data(uint16_t) for the Position Value.
-    //             uint32_t goal_position = (unsigned int)msg->position;  // Convert int32 -> uint32
+            // Position Value of X series is 4 byte data.
+            // For AX & MX(1.0) use 2 byte data(uint16_t) for the Position Value.
+            uint32_t goal_position = (unsigned int)msg->position;  // Convert int32 -> uint32
 
-    //             // Write Goal Position (length : 4 bytes)
-    //             // When writing 2 byte data to AX / MX(1.0), use write2ByteTxRx() instead.
-    //             dxl_comm_result =
-    //             packetHandler->write4ByteTxRx(
-    //                 portHandler,
-    //                 (uint8_t) msg->id,
-    //                 ADDR_GOAL_POSITION,
-    //                 goal_position,
-    //                 &dxl_error
-    //             );
+            // Write Goal Position (length : 4 bytes)
+            // When writing 2 byte data to AX / MX(1.0), use write2ByteTxRx() instead.
+            dxl_comm_result =
+            packetHandler->write4ByteTxRx(
+                portHandler,
+                (uint8_t) msg->id,
+                ADDR_GOAL_POSITION,
+                goal_position,
+                &dxl_error
+            );
 
-    //             if (dxl_comm_result != COMM_SUCCESS) {
-    //                 RCLCPP_INFO(this->get_logger(), "%s", packetHandler->getTxRxResult(dxl_comm_result));
-    //             } else if (dxl_error != 0) {
-    //                 RCLCPP_INFO(this->get_logger(), "%s", packetHandler->getRxPacketError(dxl_error));
-    //             } else {
-    //                 RCLCPP_INFO(this->get_logger(), "Set [ID: %d] [Goal Position: %d]", msg->id, msg->position);
-    //             }
-    //         }
-    //     );
+            if (dxl_comm_result != COMM_SUCCESS) {
+                RCLCPP_INFO(this->get_logger(), "%s", packetHandler->getTxRxResult(dxl_comm_result));
+            } else if (dxl_error != 0) {
+                RCLCPP_INFO(this->get_logger(), "%s", packetHandler->getRxPacketError(dxl_error));
+            } else {
+                RCLCPP_INFO(this->get_logger(), "Set [ID: %d] [Goal Position: %d]", msg->id, msg->position);
+            }
+        }
+        );
 
-    // auto get_present_position =
-    //     [this](
-    //     const std::shared_ptr<GetPosition::Request> request,
-    //     std::shared_ptr<GetPosition::Response> response) -> void
-    //     {
-    //     // Read Present Position (length : 4 bytes) and Convert uint32 -> int32
-    //     // When reading 2 byte data from AX / MX(1.0), use read2ByteTxRx() instead.
-    //     dxl_comm_result = packetHandler->read4ByteTxRx(
-    //         portHandler,
-    //         (uint8_t) request->id,
-    //         ADDR_PRESENT_POSITION,
-    //         reinterpret_cast<uint32_t *>(&present_position),
-    //         &dxl_error
-    //     );
+    auto get_present_position =
+        [this](
+        const std::shared_ptr<GetPosition::Request> request,
+        std::shared_ptr<GetPosition::Response> response) -> void
+        {
+        // Read Present Position (length : 4 bytes) and Convert uint32 -> int32
+        // When reading 2 byte data from AX / MX(1.0), use read2ByteTxRx() instead.
+        dxl_comm_result = packetHandler->read4ByteTxRx(
+            portHandler,
+            (uint8_t) request->id,
+            ADDR_PRESENT_POSITION,
+            reinterpret_cast<uint32_t *>(&present_position),
+            &dxl_error
+        );
 
-    //     RCLCPP_INFO(
-    //         this->get_logger(),
-    //         "Get [ID: %d] [Present Position: %d]",
-    //         request->id,
-    //         present_position
-    //     );
+        RCLCPP_INFO(
+            this->get_logger(),
+            "Get [ID: %d] [Present Position: %d]",
+            request->id,
+            present_position
+        );
 
-    //     response->position = present_position;
-    //     };
+        response->position = present_position;
+        };
 
-    // get_position_server_ = create_service<GetPosition>("get_position", get_present_position);
+    get_position_server_ = create_service<GetPosition>("get_position", get_present_position);
 }
 
 QuadMotorControl::~QuadMotorControl()
